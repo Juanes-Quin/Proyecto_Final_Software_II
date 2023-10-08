efc70203b6a907c8b4e8af4436c01ed9356e7587
julianscp <julians.camelop@uqvirtual.edu.co>
2023-10-08 15:55:32 -0500

    historia 8 modificada

--- proyecto_final/src/App.js
+++ proyecto_final/src/App.js
@@ -1,37 +1,40 @@
-import React, { useState } from 'react'; // Importa React y el hook useState desde React
+import React, { useState } from 'react';
+import './App.css';
 
-import './App.css'; // Importa estilos desde el archivo App.css
+const TAXI_RATE_PER_KM = 1.5;
 
-const TAXI_RATE_PER_KM = 1.5; // Tarifa por kilómetro en dólares
+function App() {
+  const [distance, setDistance] = useState('');
+  const [estimatedFare, setEstimatedFare] = useState(null);
 
-const App = () => { // Declara un componente funcional llamado App
-  const [distance, setDistance] = useState(''); // Estado para la distancia ingresada
-  const [estimatedFare, setEstimatedFare] = useState(0); // Estado para la tarifa estimada
-
-  const handleDistanceChange = (e) => { 
-    setDistance(e.target.value); // Actualiza el estado distance cuando el campo de entrada cambia
+  const handleDistanceChange = (e) => {
+    setDistance(e.target.value);
   };
 
-  const calculateFare = () => { 
-    if (!isNaN(distance)) { // Verifica si la distancia ingresada es un número válido
-      const fare = parseFloat(distance) * TAXI_RATE_PER_KM; // Calcula la tarifa multiplicando la distancia por la tarifa por kilómetro
-      setEstimatedFare(fare.toFixed(2)); // Actualiza el estado estimatedFare con el resultado redondeado a 2 decimales
+  const calculateFare = () => {
+    if (!isNaN(distance)) {
+      const fare = parseFloat(distance) * TAXI_RATE_PER_KM;
+      setEstimatedFare(fare.toFixed(2));
     } else {
-      setEstimatedFare(0); // Si la entrada no es válida, establece la tarifa en 0
+      setEstimatedFare(null);
     }
   };
 
-  return ( 
-    <div className="App"> {/* Contenedor principal con la clase CSS App */}
-      <h1>Taxi Fare Estimator</h1> {/* Encabezado */}
-      <label>
-        Distance (in kilometers):
-        <input type="text" value={distance} onChange={handleDistanceChange} /> {/* Campo de entrada controlado por el estado distance */}
-      </label>
-      <button onClick={calculateFare}>Calculate Fare</button> {/* Botón para calcular la tarifa */}
-      <h2>Estimated Fare: ${estimatedFare}</h2> {/* Muestra la tarifa estimada */}
+  return (
+    <div className="App">
+      <h1>Taxi Fare Estimator</h1>
+      <div className="input-container">
+        <label>
+          Distance (in kilometers):
+          <input type="text" value={distance} onChange={handleDistanceChange} />
+        </label>
+      </div>
+      <button onClick={calculateFare}>Calculate Fare</button>
+      {estimatedFare !== null && (
+        <div className="result">Estimated Fare: ${estimatedFare}</div>
+      )}
     </div>
   );
-};
+}
 
-export default App; // Exporta el componente App para su uso en otros archivos
+export default App;
